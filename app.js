e6821eaf9d1efb6c978b14ee3167140342a11503
Juanki420 <129750609+Juanki420@users.noreply.github.com>
2023-11-18 22:29:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var firebaseConfig = {
     apiKey: "AIzaSyC5FR4fLXV1zjAzZ4WFIwBG97Aes3FtPWo",
     authDomain: "contador-c6528.firebaseapp.com",
-    databaseURL: "https://console.firebase.google.com/u/0/project/contador-c6528/database/contador-c6528-default-rtdb/data/~2F?hl=es-419",
+    databaseURL: "https://contador-c6528.firebaseio.com",
     projectId: "contador-c6528",
     storageBucket: "contador-c6528.appspot.com",
     messagingSenderId: "575749501934",
@@ -23,25 +23,3 @@ nameRef.on('child_added', function(data) {
     li.innerText = data.val();
     nameList.appendChild(li);
 });
-
-
-
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyC5FR4fLXV1zjAzZ4WFIwBG97Aes3FtPWo",
-    authDomain: "contador-c6528.firebaseapp.com",
-    projectId: "contador-c6528",
-    storageBucket: "contador-c6528.appspot.com",
-    messagingSenderId: "575749501934",
-    appId: "1:575749501934:web:4b48ebab36b25e925914ff"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
