7f1647e4c8839fbe40a4a9ce7195d038dd06a788
Juanki420 <129750609+Juanki420@users.noreply.github.com>
2023-11-19 00:01:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
 // app.js
 
 var firebaseConfig = {
-    apiKey: "AIzaSyC5FR4fLXV1zjAzZ4WFIwBG97Aes3FtPWo",
-  authDomain: "contador-c6528.firebaseapp.com",
-  databaseURL: "https://contador-c6528-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "contador-c6528",
-  storageBucket: "contador-c6528.appspot.com",
-  messagingSenderId: "575749501934",
-  appId: "1:575749501934:web:4b48ebab36b25e925914ff"
+    // Tu configuración de Firebase aquí
 };
 
 firebase.initializeApp(firebaseConfig);
 
 var nameList = document.getElementById('nameList');
 var nameRef = firebase.database().ref('names');
-
-// Usar 'child_added' para cargar nombres existentes al inicio y en tiempo real
-nameRef.on('child_added', function(data) {
-    var li = document.createElement('li');
-    li.innerText = data.val();
-    nameList.appendChild(li);
-});
-
-// Verificar si se ha enviado un nombre almacenado en el localStorage
-var hasSubmittedName = localStorage.getItem('hasSubmittedName') === 'true';
+var canSubmitNames = true;
 
 function handleFormSubmission() {
     // Verificar si ya se ha enviado un nombre
@@ -50,4 +35,28 @@ function handleFormSubmission() {
     nameInput.value = '';
 }
 
+function resetNameSubmissions() {
+    if (firebase.auth().currentUser) {
+        // Solo permitir el restablecimiento si hay un usuario autenticado
+        canSubmitNames = true;
+        alert('Ahora puedes enviar nombres nuevamente.');
+    } else {
+        alert('Debes iniciar sesión para restablecer los envíos de nombres.');
+    }
+}
+
 document.getElementById('submitButton').addEventListener('click', handleFormSubmission);
+document.getElementById('resetButton').addEventListener('click', resetNameSubmissions);
+
+document.getElementById('loginButton').addEventListener('click', function() {
+    // Abrir el cuadro de diálogo de inicio de sesión cuando se hace clic en el botón de inicio de sesión
+    firebase.auth().signInWithEmailAndPassword('tu@email.com', 'tuContraseña')
+        .then(function(userCredential) {
+            // El usuario ha iniciado sesión correctamente
+            alert('¡Has iniciado sesión correctamente!');
+        })
+        .catch(function(error) {
+            // Manejar errores de inicio de sesión
+            alert('Error al iniciar sesión: ' + error.message);
+        });
+});
