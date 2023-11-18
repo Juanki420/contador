877274de087d61419803dfa681d82966069f4b40
Juanki420 <129750609+Juanki420@users.noreply.github.com>
2023-11-19 00:35:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 
 var firebaseConfig = {
     apiKey: "AIzaSyC5FR4fLXV1zjAzZ4WFIwBG97Aes3FtPWo",
-  authDomain: "contador-c6528.firebaseapp.com",
-  databaseURL: "https://contador-c6528-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "contador-c6528",
-  storageBucket: "contador-c6528.appspot.com",
-  messagingSenderId: "575749501934",
-  appId: "1:575749501934:web:4b48ebab36b25e925914ff"
+    authDomain: "contador-c6528.firebaseapp.com",
+    databaseURL: "https://contador-c6528-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "contador-c6528",
+    storageBucket: "contador-c6528.appspot.com",
+    messagingSenderId: "575749501934",
+    appId: "1:575749501934:web:4b48ebab36b25e925914ff"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -15,6 +15,23 @@ firebase.initializeApp(firebaseConfig);
 var nameList = document.getElementById('nameList');
 var nameRef = firebase.database().ref('names');
 var canSubmitNames = true;
+
+// Manejar tanto nuevos nombres como cambios en los nombres existentes
+nameRef.on('child_added', handleNameChange);
+nameRef.on('child_changed', handleNameChange);
+
+function handleNameChange(data) {
+    var li = document.createElement('li');
+    li.innerText = data.val();
+    nameList.appendChild(li);
+}
+
+// Verificar si el usuario actual es el propietario permitido
+function isAllowedUser() {
+    var user = firebase.auth().currentUser;
+    // Aquí deberías reemplazar 'TU_ID_DE_GOOGLE' con tu propio ID de Google
+    return user && user.providerData[0]?.providerId === 'google.com' && user.uid === 'TU_ID_DE_GOOGLE';
+}
 
 function handleFormSubmission() {
     // Verificar si se pueden enviar nombres
@@ -41,14 +58,11 @@ function handleFormSubmission() {
 }
 
 function resetNameSubmissions() {
-    var user = firebase.auth().currentUser;
-
-    if (user) {
-        // Solo permitir el restablecimiento si hay un usuario autenticado
+    if (isAllowedUser()) {
         canSubmitNames = true;
         alert('Ahora puedes enviar nombres nuevamente.');
     } else {
-        alert('Debes iniciar sesión para restablecer los envíos de nombres.');
+        alert('No tienes permisos para restablecer los envíos de nombres.');
     }
 }
 
