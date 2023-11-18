86d79c9a712c0b92639b50dcafd50201c5422bce
Juanki420 <129750609+Juanki420@users.noreply.github.com>
2023-11-18 23:24:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 // app.js
 
 var firebaseConfig = {
-    apiKey: "AIzaSyC5FR4fLXV1zjAzZ4WFIwBG97Aes3FtPWo",
-  authDomain: "contador-c6528.firebaseapp.com",
-  databaseURL: "https://contador-c6528-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "contador-c6528",
-  storageBucket: "contador-c6528.appspot.com",
-  messagingSenderId: "575749501934",
-  appId: "1:575749501934:web:4b48ebab36b25e925914ff"
+    // Tu configuración de Firebase aquí
 };
 
 firebase.initializeApp(firebaseConfig);
+
+// Definir la función submitForm
+function submitForm() {
+    var name = document.getElementById('nameInput').value;
+    if (name.trim() !== '') {
+        firebase.database().ref('names').push(name);
+        document.getElementById('nameInput').value = '';
+    }
+}
 
 var nameList = document.getElementById('nameList');
 var nameRef = firebase.database().ref('names');
@@ -24,9 +27,5 @@ nameRef.on('child_added', function(data) {
 
 document.getElementById('nameForm').addEventListener('submit', function(e) {
     e.preventDefault();
-    var name = document.getElementById('nameInput').value;
-    if (name.trim() !== '') {
-        firebase.database().ref('names').push(name);
-        document.getElementById('nameInput').value = '';
-    }
+    submitForm();  // Llamar directamente a submitForm aquí
 });
