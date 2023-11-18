91c7b09d484e1bc9f7c38dbe34c0b4e2f0c16787
Juanki420 <129750609+Juanki420@users.noreply.github.com>
2023-11-18 22:47:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyC5FR4fLXV1zjAzZ4WFIwBG97Aes3FtPWo",
-    authDomain: "contador-c6528.firebaseapp.com",
-    databaseURL: "https://contador-c6528.firebaseio.com",
-    projectId: "contador-c6528",
-    storageBucket: "contador-c6528.appspot.com",
-    messagingSenderId: "575749501934",
-    appId: "1:575749501934:web:4b48ebab36b25e925914ff"
+  apiKey: "AIzaSyC5FR4fLXV1zjAzZ4WFIwBG97Aes3FtPWo",
+  authDomain: "contador-c6528.firebaseapp.com",
+  databaseURL: "https://contador-c6528.firebaseio.com",
+  projectId: "contador-c6528",
+  storageBucket: "contador-c6528.appspot.com",
+  messagingSenderId: "575749501934",
+  appId: "1:575749501934:web:4b48ebab36b25e925914ff"
 };
+
 firebase.initializeApp(firebaseConfig);
 
 document.getElementById('nameForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    var name = document.getElementById('nameInput').value;
-    firebase.database().ref('names').push(name);
-    document.getElementById('nameInput').value = '';
+  e.preventDefault();
+  var name = document.getElementById('nameInput').value;
+  firebase.database().ref('names').push({
+    name: name
+  });
+  document.getElementById('nameInput').value = '';
 });
 
 var nameList = document.getElementById('nameList');
 var nameRef = firebase.database().ref('names');
-nameRef.on('child_added', function(data) {
-    var li = document.createElement('li');
-    li.innerText = data.val();
-    nameList.appendChild(li);
+nameRef.on('child_added', function(snapshot) {
+  var name = snapshot.val().name;
+  var li = document.createElement('li');
+  li.innerText = name;
+  nameList.appendChild(li);
 });
