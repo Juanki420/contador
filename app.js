c7aea239ad7c83ea8f2171d1088aca708887a4e8
Juanki420 <129750609+Juanki420@users.noreply.github.com>
2023-11-19 20:41:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -60,10 +60,7 @@ function handleFormSubmission(e) {
     hasUserSubmittedMessage(user.uid).then(function(hasSubmitted) {
         if (hasSubmitted) {
             alert('Ya has enviado un mensaje. No puedes enviar otro.');
-        } else {
-            if (!canSubmitNames) {
-                alert('Los envíos de nombres están deshabilitados en este momento.');
-                return;
+        } 
             }
 
             var nameInput = document.getElementById('nameInput');
