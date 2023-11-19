f95868d30bc477c30930ee806095ea68ff91ac82
Juanki420 <129750609+Juanki420@users.noreply.github.com>
2023-11-19 04:00:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-var _0x1a93=["\x61\x70\x69\x4B\x65\x79","\x64\x61\x74\x61\x62\x61\x73\x65\x55\x52\x4C","\x63\x6F\x6E\x66\x69\x67","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x63\x6F\x6E\x74\x61\x64\x6F\x72\x2D\x63\x36\x35\x32\x38\x2E\x66\x69\x72\x65\x62\x61\x73\x65\x64\x61\x74\x61\x62\x61\x73\x65\x2E\x61\x70\x70","\x70\x72\x6F\x6A\x65\x63\x74\x49\x64","\x63\x36\x35\x32\x38","\x65\x75\x72\x6F\x70\x65","\x61\x6E\x6F\x6E\x79\x6D\x6F\x75\x73","\x76\x61\x6C","\x6E\x61\x6D\x65\x4C\x69\x73\x74","\x72\x65\x66","\x6E\x61\x6D\x65","\x68\x61\x6E\x64\x6C\x65\x4E\x61\x6D\x65","\x63\x68\x69\x6C\x64","\x76\x61\x6C\x75\x65","\x72\x65\x66\x4F\x6E","\x6E\x61\x6D\x65\x49\x6E\x70\x75\x74","\x72\x65\x66\x4E\x6F\x64\x65\x73","\x73\x75\x62\x6D\x69\x74","\x63\x68\x69\x6C\x64\x5F\x61\x64\x64\x65\x64","\x76\x61\x6C\x75\x65\x4F\x66","\x67\x6F\x6F\x67\x6C\x65\x2E\x63\x6F\x6D\x2F\x75\x2F\x30\x2F\x70\x72\x6F\x6A\x65\x63\x74\x2F\x63\x6F\x75\x6E\x74\x61\x64\x6F\x72\x2D\x63\x36\x35\x32\x38\x2F\x64\x61\x74\x61\x62\x61\x73\x65\x2F\x63\x6F\x75\x6E\x74\x61\x64\x6F\x72\x2D\x63\x36\x35\x32\x38\x2D\x64\x65\x66\x61\x75\x6C\x74\x2D\x72\x74\x64\x62\x2F\x64\x61\x74\x61\x2F\x7E\x32\x46","\x6E\x61\x6D\x65\x49\x6E\x70\x75\x74","\x6E\x61\x6D\x65\x49\x6E\x70\x75\x74","\x76\x61\x6C\x75\x65","\x73\x75\x62\x6D\x69\x74","\x63\x68\x69\x6C\x64\x5F\x61\x64\x64\x65\x64","\x73\x75\x62\x6D\x69\x74\x4E\x61\x6D\x65","\x73\x75\x62\x6D\x69\x74\x74\x65\x64\x4E\x61\x6D\x65","\x73\x75\x62\x6D\x69\x74\x74\x65\x64\x4E\x61\x6D\x65","\x64\x69\x73\x70\x6C\x61\x79\x55\x73\x65\x72\x49\x6E\x66\x6F","\x4D\x68\x61\x73\x20\x69\x6E\x69\x63\x69\x61\x64\x6F\x20\x63\x6F\x72\x72\x65\x63\x74\x61\x6D\x65\x6E\x74\x65\x3A\x20","\x63\x68\x69\x6C\x64","\x69\x6E\x6E\x65\x72\x54\x65\x78\x74","\x62\x6F\x64\x79","\x42\x69\x65\x6E\x76\x65\x6E\x69\x64\x6F\x20\x61\x20\x6D\x69\x20\x70\xC3\xA1\x67\x69\x6E\x61\x20\x77\x65\x62\x21","\x4E"]; 
+// app.js
 
-firebase.initializeApp({
-    apiKey: _0x1a93[0],
-    authDomain: _0x1a93[1],
-    databaseURL: _0x1a93[2],
-    projectId: _0x1a93[3],
-    storageBucket: _0x1a93[4],
-    messagingSenderId: _0x1a93[5],
-    appId: _0x1a93[6]
-});
+var firebaseConfig = {
+    apiKey: "AIzaSyC5FR4fLXV1zjAzZ4WFIwBG97Aes3FtPWo",
+    authDomain: "contador-c6528.firebaseapp.com",
+    databaseURL: "https://contador-c6528-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "contador-c6528",
+    storageBucket: "contador-c6528.appspot.com",
+    messagingSenderId: "575749501934",
+    appId: "1:575749501934:web:4b48ebab36b25e925914ff"
+};
 
-var nameList = document.getElementById(_0x1a93[7]);
-var nameRef = firebase.database().ref(_0x1a93[8]);
+firebase.initializeApp(firebaseConfig);
+
+var nameList = document.getElementById('nameList');
+var nameRef = firebase.database().ref('names');
 var canSubmitNames = true;
 
-nameRef.on(_0x1a93[9], handleNameChange);
-nameRef.on(_0x1a93[10], handleNameChange);
+// Manejar tanto nuevos nombres como cambios en los nombres existentes
+nameRef.on('child_added', handleNameChange);
+nameRef.on('child_changed', handleNameChange);
 
 function handleNameChange(data) {
     var li = document.createElement('li');
@@ -23,19 +26,24 @@ function handleNameChange(data) {
     nameList.appendChild(li);
 }
 
+// Verificar si el usuario actual es el propietario permitido
 function isAllowedUser() {
     var user = firebase.auth().currentUser;
+    // Aquí deberías reemplazar 'TU_ID_DE_GOOGLE' con tu propio ID de Google
     return user && user.providerData[0]?.providerId === 'google.com' && user.uid === 'EcjgireoyRNjZ7Fo3W3eMZT05jp1';
 }
 
+// Verificar si el usuario ya ha enviado un nombre
 function hasSubmittedName() {
-    return localStorage.getItem(_0x1a93[11]) === 'true';
+    return localStorage.getItem('submittedName') === 'true';
 }
 
+// Almacenar que el usuario ha enviado un nombre
 function setSubmittedName() {
-    localStorage.setItem(_0x1a93[11], 'true');
+    localStorage.setItem('submittedName', 'true');
 }
 
+// Mostrar información del usuario en la página
 function displayUserInfo(user) {
     var userInfoElement = document.getElementById('userInfo');
     if (userInfoElement) {
@@ -44,13 +52,16 @@ function displayUserInfo(user) {
 }
 
 function handleFormSubmission(e) {
+    // Prevenir el comportamiento predeterminado del formulario (recarga de la página)
     e.preventDefault();
 
+    // Verificar si el usuario ya ha enviado un nombre
     if (hasSubmittedName()) {
         alert('Solo puedes enviar un nombre.');
         return;
     }
 
+    // Verificar si se pueden enviar nombres
     if (!canSubmitNames) {
         alert('Los envíos de nombres están deshabilitados en este momento.');
         return;
@@ -59,14 +70,20 @@ function handleFormSubmission(e) {
     var nameInput = document.getElementById('nameInput');
     var name = nameInput.value.trim();
 
+    // Validar la longitud del nombre
     if (name.length === 0 || name.length > 30) {
         alert('Por favor, ingresa un nombre válido (máximo 30 caracteres).');
         return;
     }
 
+    // Deshabilitar envíos de nombres después de enviar uno
     canSubmitNames = false;
+
+    // Enviar el nombre a Firebase
     firebase.database().ref('names').push(name);
     nameInput.value = '';
+
+    // Almacenar que el usuario ha enviado un nombre
     setSubmittedName();
 }
 
@@ -74,43 +91,54 @@ function resetNameSubmissions() {
     if (isAllowedUser()) {
         canSubmitNames = true;
         alert('Ahora puedes enviar nombres nuevamente.');
-        localStorage.removeItem(_0x1a93[11]);
+        // Eliminar la marca de que el usuario ha enviado un nombre
+        localStorage.removeItem('submittedName');
     } else {
         alert('No tienes permisos para restablecer los envíos de nombres.');
     }
 }
 
-var loginButton = document.getElementById(_0x1a93[12]);
+// Verificar si el botón existe antes de agregar el evento
+var loginButton = document.getElementById('loginButton');
 if (loginButton) {
-    loginButton.addEventListener(_0x1a93[13], function() {
-        var provider = new firebase.auth.GoogleAuthProvider();
+    loginButton.addEventListener('click', function() {
+        // Abrir el cuadro de diálogo de inicio de sesión cuando se hace clic en el botón de inicio de sesión
+        var provider = new firebase.auth.GoogleAuthProvider(); // Cambiado a GoogleAuthProvider
+
+        // Cambiar signInWithRedirect a signInWithPopup para Firebase 8.x
         firebase.auth().signInWithPopup(provider)
             .then(function(result) {
+                // El usuario ha iniciado sesión correctamente
                 alert('¡Has iniciado sesión correctamente!');
             })
             .catch(function(error) {
+                // Manejar errores de inicio de sesión
                 alert('Error al iniciar sesión: ' + error.message);
             });
     });
 }
 
-document.getElementById('submitButton').addEventListener(_0x1a93[13], handleFormSubmission);
-document.getElementById('resetButton').addEventListener(_0x1a93[13], resetNameSubmissions);
+document.getElementById('submitButton').addEventListener('click', handleFormSubmission);
+document.getElementById('resetButton').addEventListener('click', resetNameSubmissions);
 
 var logoutButton = document.getElementById('logoutButton');
 if (logoutButton) {
-    logoutButton.addEventListener(_0x1a93[13], function() {
+    logoutButton.addEventListener('click', function() {
         firebase.auth().signOut().then(function() {
+            // Cierre de sesión exitoso
             alert('Has cerrado sesión correctamente.');
         }).catch(function(error) {
+            // Manejar errores de cierre de sesión
             alert('Error al cerrar sesión: ' + error.message);
         });
     });
 }
 
+// Actualizar la información del usuario al iniciar o cerrar sesión
 firebase.auth().onAuthStateChanged(function(user) {
     displayUserInfo(user);
 
+    // Mostrar o ocultar botones según el estado de inicio de sesión
     if (user) {
         loginButton.style.display = 'none';
         logoutButton.style.display = 'block';
