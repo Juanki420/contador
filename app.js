8d6e09318960977673ebfc9b4eeb81cec17bfbde
Juanki420 <129750609+Juanki420@users.noreply.github.com>
2023-11-19 21:22:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -223,5 +223,4 @@ document.getElementById('resetNamesButton').addEventListener('click', resetNames
 document.getElementById('resetButton').addEventListener('click', resetAllData);
 document.getElementById('logoutButton').addEventListener('click', logout);
 document.getElementById('loginButton').addEventListener('click', loginWithGoogle);
-document.getElementById('emailLoginButton').addEventListener('click', loginWithEmail);
 document.getElementById('registerButton').addEventListener('click', register);
