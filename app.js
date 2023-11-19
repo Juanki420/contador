1ea900b34d07b76fe69abbe47b3b66571fbc9ba7
Juanki420 <129750609+Juanki420@users.noreply.github.com>
2023-11-19 01:46:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,9 +12,6 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-// Importar módulo de autenticación de Firebase
-import 'firebase/auth';
-
 var nameList = document.getElementById('nameList');
 var nameRef = firebase.database().ref('names');
 var canSubmitNames = true;
@@ -122,4 +119,9 @@ if (loginButton) {
 }
 
 document.getElementById('submitButton').addEventListener('click', handleFormSubmission);
-document.getElementById('resetButton').addEventListener('click',
+document.getElementById('resetButton').addEventListener('click', resetNameSubmissions);
+
+// Actualizar la información del usuario al iniciar o cerrar sesión
+firebase.auth().onAuthStateChanged(function(user) {
+    displayUserInfo(user);
+});
